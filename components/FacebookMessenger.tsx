9e89aa16524f5aa852bacf151f98e1a8a7a44179
tourkidisgiannis <tourkidisgiannis@gmail.com>
2025-12-13 20:28:11 +0200

    feat:messender added

--- components/FacebookMessenger.tsx
+++ components/FacebookMessenger.tsx
@@ -3,38 +3,27 @@
 import Script from "next/script";
 
 export function FacebookMessenger() {
+  // Only render if FACEBOOK_PAGE_ID is defined
+  if (!process.env.FACEBOOK_PAGE_ID) {
+    return null;
+  }
+
+  const fbScriptUrl = `https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js`;
+
   return (
     <>
       <div id="fb-root"></div>
       <div
-        id="fb-customer-chat"
         className="fb-customerchat"
+        data-page_id={process.env.FACEBOOK_PAGE_ID}
+        data-attribution="setup_tool"
       ></div>
       <Script
-        id="messenger-tag"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            var chatbox = document.getElementById('fb-customer-chat');
-            chatbox.setAttribute("page_id", "YOUR_PAGE_ID");
-            chatbox.setAttribute("attribution", "biz_inbox");
-
-            window.fbAsyncInit = function() {
-              FB.init({
-                xfbml            : true,
-                version          : 'v18.0'
-              });
-            };
-
-            (function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-          `,
-        }}
+        src={fbScriptUrl}
+        strategy="lazyOnload"
+        crossOrigin="anonymous"
+        async
+        defer
       />
     </>
   );
---
+++ lib/types/facebook.d.ts
@@ -0,0 +1,8 @@
+// Declare the Facebook SDK types
+declare global {
+  interface Window {
+    FB: any;
+  }
+}
+
+export {};
