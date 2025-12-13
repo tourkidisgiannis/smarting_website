1ead7499ac5f7a963f52c14bfc8b23628f15c32d
tourkidisgiannis <tourkidisgiannis@gmail.com>
2025-12-13 21:44:22 +0200

    feat:messender added 7

--- components/FacebookMessenger.tsx
+++ components/FacebookMessenger.tsx
@@ -1,29 +1,50 @@
-"use client";
+import { useEffect } from "react";
 
-import Script from "next/script";
+const FacebookMessenger = () => {
+  useEffect(() => {
+    // Only initialize if page ID is available
+    if (!process.env.NEXT_PUBLIC_FACEBOOK_PAGE_ID) return;
 
-export function FacebookMessenger() {
-  // Only render if NEXT_PUBLIC_FACEBOOK_PAGE_ID is defined
+    // Check if Facebook SDK is already loaded
+    if (window.FB) return;
+
+    // Create fb-root div if it doesn't exist
+    if (!document.getElementById("fb-root")) {
+      const fbRoot = document.createElement("div");
+      fbRoot.id = "fb-root";
+      document.body.appendChild(fbRoot);
+    }
+
+    // Set up Facebook SDK initialization
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        xfbml: true,
+        version: "v18.0",
+      });
+    };
+
+    // Load Facebook Customer Chat SDK
+    const script = document.createElement("script");
+    script.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+    script.async = true;
+    script.defer = true;
+    script.crossOrigin = "anonymous";
+    script.id = "facebook-jssdk";
+    document.body.appendChild(script);
+  }, []);
+
+  // Only render if page ID is available
   if (!process.env.NEXT_PUBLIC_FACEBOOK_PAGE_ID) {
     return null;
   }
 
-  const fbPageId = process.env.NEXT_PUBLIC_FACEBOOK_PAGE_ID;
-
   return (
-    <>
-      <div id="fb-root"></div>
-      <div
-        className="fb-customerchat"
-        data-page_id={fbPageId}
-        data-attribution="setup_tool"
-      ></div>
-      <Script
-        src={`https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js`}
-        strategy="afterInteractive"
-        crossOrigin="anonymous"
-        id="facebook-jssdk"
-      />
-    </>
+    <div
+      className="fb-customerchat"
+      data-page_id={process.env.NEXT_PUBLIC_FACEBOOK_PAGE_ID}
+      data-attribution="setup_tool"
+    />
   );
-}
+};
+
+export default FacebookMessenger;
